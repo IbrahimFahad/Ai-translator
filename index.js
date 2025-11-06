c64c0c4d1f8e2571685680314e1320da2e1c560b
Ibrahim Fahad <98151674+IbrahimFahad@users.noreply.github.com>
2025-11-06 23:52:59 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,65 @@
+import OpenAi from 'openai';
+
+function handleSubmit() {
+  const input_text = document.querySelector(".input-form").value;
+  const selected_lan = document.querySelector('input[name="language"]:checked').value;
+  return { input_text, selected_lan };
+}
+
+
+
+async function goToPage() {
+  const { input_text, selected_lan } = handleSubmit();
+  const text = await transleted(input_text,selected_lan); 
+  localStorage.setItem("originalText", input_text);
+  localStorage.setItem("text", text);
+  window.location.href = "Results_view.html";
+  
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  const originalBox = document.querySelector(".input-form");
+  const outputBox = document.getElementById("input-form-2");
+
+  if (originalBox && outputBox) {
+    originalBox.value = localStorage.getItem("originalText") || "";
+    outputBox.value = localStorage.getItem("text") || "";
+  }
+});
+
+async function transleted(input_text,selected_lan)
+{
+    const client = new OpenAi ({
+    
+     dangerouslyAllowBrowser: true
+})
+
+const messages = [
+    {
+        role:"system",
+        content:"You are an expert multilingual translator specializing in French, Spanish, and Japanese. When the user provides text and specifies a target language"
+    },
+    {
+        role:"user",
+        content: `${input_text} -> ${selected_lan}`
+
+    }
+]
+
+const response = await client.chat.completions.create({
+    
+    model:"gpt-4o-mini",
+    messages:messages
+    
+    
+})
+  return response.choices[0].message.content;
+}
+
+window.goToPage = goToPage;
+    
+
+
+
+    
+    
